32142012b4efd07fa2ea69b23728e2e83ddba7d4
novaeye <fcoffee@gmail.com>
2013-10-26 14:34:10 +0800

    change for redmine2.x

---
+++ lib/hooks/view_layouts_base_html_head_hook.rb
@@ -0,0 +1,22 @@
+module Redmine
+  module SyntaxHighlighting
+    module JSHighter
+
+      class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
+        def view_layouts_base_html_head(context)
+          if context[:controller]
+            #js_path  = ActionView::Helpers::AssetTagHelper.plugin_asset_path('redmine_js_syntax_highlighter', 'javascripts', '')
+            js_path =  "#{Redmine::Utils.relative_url_root}/plugin_assets/redmine_js_syntax_highlighter/javascripts/"
+            js_theme = Redmine::SyntaxHighlighting::JSHighter.theme != '' ?  "Default" :  Redmine::SyntaxHighlighting::JSHighter.theme
+            stylesheet_link_tag('shCore.css', :plugin => 'redmine_js_syntax_highlighter') +
+            stylesheet_link_tag("shTheme#{js_theme}.css", :plugin => "redmine_js_syntax_highlighter") +
+            javascript_include_tag('shCore.js', :plugin => 'redmine_js_syntax_highlighter') +
+            javascript_include_tag('shAutoloader.js', :plugin => 'redmine_js_syntax_highlighter') +
+            javascript_include_tag('shAutoLoadAll.js', :plugin => 'redmine_js_syntax_highlighter') +
+            javascript_tag("$(document).ready(function () {shLoadAllSyntaxHighlighters(\"#{js_path}\");});")
+          end
+        end
+      end
+    end
+  end
+end
--- lib/js_syntax_patch.rb
+++ lib/js_syntax_patch.rb
@@ -17,7 +17,7 @@ module Redmine
       class << self
         def highlight_by_filename(text, filename, encoding=true)
           require 'coderay/helpers/file_type'
-          language = ::CodeRay::FileType.fetch(filename, :plaintext)
+          language = ::CodeRay::FileType[filename]
           text = ERB::Util.h(text) if encoding
           language = :plaintext if [:yaml, :scheme, :debug].include?(language) 
           "<pre class=\"brush: #{language};\">#{text}</pre>"
@@ -32,21 +32,6 @@ module Redmine
         def theme
             ## User selection of highlighter Theme
             User.current.custom_value_for(CustomField.first(:conditions => {:name => 'JSHighter Theme'})) || JSHighter::DEFAULT_THEME
-        end
-      end
-      
-      class Assets < Redmine::Hook::ViewListener
-        def view_layouts_base_html_head(context)
-          if context[:controller]
-            js_path  = Engines::RailsExtensions::AssetHelpers.plugin_asset_path('redmine_js_syntax_highlighter', 'javascripts', '')
-            js_theme = Redmine::SyntaxHighlighting::JSHighter.theme != '' ?  "Default" :  Redmine::SyntaxHighlighting::JSHighter.theme
-            stylesheet_link_tag('shCore.css', :plugin => 'redmine_js_syntax_highlighter') +
-            stylesheet_link_tag("shTheme#{js_theme}.css", :plugin => "redmine_js_syntax_highlighter") +
-            javascript_include_tag('shCore.js', :plugin => 'redmine_js_syntax_highlighter') +
-            javascript_include_tag('shAutoloader.js', :plugin => 'redmine_js_syntax_highlighter') +
-            javascript_include_tag('shAutoLoadAll.js', :plugin => 'redmine_js_syntax_highlighter') +
-            javascript_tag("document.observe(\"dom:loaded\", function () {shLoadAllSyntaxHighlighters(\"#{js_path}\");});")
-          end
         end
       end
     end
